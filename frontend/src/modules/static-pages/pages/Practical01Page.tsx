efeb82fc24be09f8cd9a332cb506d0d2e424aab2
Tomáš Horáček <tomas.horacek@gmail.com>
2023-09-22 09:30:25 +0200

    Practical 01 final

--- frontend/src/modules/static-pages/pages/Practical01Page.tsx
+++ frontend/src/modules/static-pages/pages/Practical01Page.tsx
@@ -1,11 +1,39 @@
-import { Box, Button, Heading, Paragraph } from 'src/shared/design-system';
+import { useState } from 'react';
+
+import {
+  Box,
+  Button,
+  Heading,
+  Paragraph,
+  Stack,
+} from 'src/shared/design-system';
+
+const INITIAL_COUNTER = 0;
 
 export function Practical01Page() {
+  const [counter, setCounter] = useState(INITIAL_COUNTER);
+
   return (
     <Box>
       <Heading>Practical 01</Heading>
-      <Paragraph>Hello!</Paragraph>
-      <Button onClick={() => alert('Button Pressed!')}>Press Me</Button>
+      <Paragraph>Counter: {counter}</Paragraph>
+      <Stack direction="row">
+        <Button
+          colorScheme="blue"
+          onClick={() => setCounter((counter) => counter - 1)}
+        >
+          -1
+        </Button>
+        <Button
+          colorScheme="blue"
+          onClick={() => setCounter((counter) => counter + 1)}
+        >
+          +1
+        </Button>
+        <Button colorScheme="red" onClick={() => setCounter(INITIAL_COUNTER)}>
+          Reset
+        </Button>
+      </Stack>
     </Box>
   );
 }
